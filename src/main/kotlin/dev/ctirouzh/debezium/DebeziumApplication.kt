d1c9c95e19631108145b2fa5026ea9630569d224
ctirouzh <ctirouzh@gmail.com>
2025-09-20 22:00:12 +0330

    init...

---
+++ src/main/kotlin/dev/ctirouzh/debezium/DebeziumApplication.kt
@@ -0,0 +1,11 @@
+package dev.ctirouzh.debezium
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DebeziumApplication
+
+fun main(args: Array<String>) {
+  runApplication<DebeziumApplication>(*args)
+}
